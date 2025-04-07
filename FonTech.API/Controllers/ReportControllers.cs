8035def44a63db6081d9cc19f354e91dbefc4fcb
solyi <solyi@PCSOLIK>
2025-04-07 04:49:03 +0300

    Add project files.

---
+++ FonTech.API/Controllers/ReportControllers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FonTech.API.Controllers
+{
+    public class ReportControllers : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
